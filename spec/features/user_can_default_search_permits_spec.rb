0a1cac0aca19862667ceea3c086ec3ad03db4eb5
Megan Arellano <ssciolist@gmail.com>
2018-05-24 15:38:42 -0600

    applies local styling to search landing text

--- spec/features/user_can_default_search_permits_spec.rb
+++ spec/features/user_can_default_search_permits_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
-describe 'As a visitor' do
+describe 'As a visitor,' do
   describe 'when I visit the homepage' do
-    describe 'and enter a Denver address in the search bar' do
-      it 'shows me a figure and first 15 permit results' do
+    describe 'and enter a Denver address in the search bar,' do
+      it 'shows me a figure and permit results within one mile' do
         visit '/'
 
-        fill_in 'search-form', with: '2035 N JASMINE ST, Denver Colorado'
+        fill_in 'search', with: '2035 N JASMINE ST, Denver Colorado'
+        click_on 'Search'
 
         expect(current_path).to eq('/search')
         expect(page).to have_content('Approximately $770,690 in construction was planned within 1 mile')
