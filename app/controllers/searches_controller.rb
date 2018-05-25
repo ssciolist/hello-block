67f2845d2cee1136086fbe87dbabf449206e7917
Megan Arellano <ssciolist@gmail.com>
2018-05-24 20:01:48 -0600

    adds styling and view helper to results table

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,7 @@
 class SearchesController < ApplicationController
   def index
     @presenter = BuildingPermitSearchPresenter.new
-    @search = BuildingPermit.where(date_issued: (DateTime.now - params[:days].to_i)..(DateTime.now)).near(params[:search], params[:distance])
+    @search = BuildingPermit.near(params[:search], params[:distance])
     @value = @search.sum(:valuation)
     @distance = params[:distance]
     @days = params[:days]
