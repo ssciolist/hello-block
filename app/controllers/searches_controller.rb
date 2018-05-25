33343de0a39d1bab43a4e2419e580284a590e391
Megan Arellano <ssciolist@gmail.com>
2018-05-24 18:31:44 -0600

    removes duplicative class method test

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,7 @@ class SearchesController < ApplicationController
   def index
     @presenter = BuildingPermitSearchPresenter.new
     @value = BuildingPermit.near(params[:search]).sum(:valuation)
+    
 
   end
 end
