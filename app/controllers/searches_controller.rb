3d722bbf8ff0838e9df9728bc8fb1cc62c03c153
Megan Arellano <ssciolist@gmail.com>
2018-05-24 19:04:40 -0600

    resolves testing difficulties, adds search functionality

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,10 @@
 class SearchesController < ApplicationController
   def index
     @presenter = BuildingPermitSearchPresenter.new
-    @value = BuildingPermit.near(params[:search]).sum(:valuation)
-    
+    @search = BuildingPermit.near(params[:search], params[:distance])
+    @value = BuildingPermit.near(params[:search], params[:distance]).sum(:valuation)
+    @distance = params[:distance]
+
 
   end
 end
--- spec/features/user_can_default_search_permits_spec.rb
+++ spec/features/user_can_default_search_permits_spec.rb
@@ -5,8 +5,8 @@ describe 'As a visitor,' do
     describe 'and enter a Denver address in the search bar,' do
       it 'shows me a figure and permit results within one mile' do
         permit = PermitType.create(name: "New home", p_type: "003")
-        BuildingPermit.create(date_issued: "2015-04-06 11:38:17", permit_number: "24", address: "123 street", valuation: 770690, fee: 4, owner_name: "JASMINE", contractor_name: "besine", permit_type: permit, latitude: 39.74863, longitude: -104.91841)
-        BuildingPermit.create(date_issued: "2015-04-06 11:38:17", permit_number: "324", address: "3123 street", valuation: 0, fee: 34, owner_name: "JASMINE", contractor_name: "besine", permit_type: permit, latitude: 39.7455399339223, longitude: -104.92825913894)
+        BuildingPermit.create(date_issued: "2015-04-06 11:38:17", permit_number: "24", address: "2035 North Jasmine Street", valuation: 770689, fee: 4, owner_name: "JASMINE", contractor_name: "besine", permit_type: permit, latitude: 39.748276, longitude: -104.918953)
+        BuildingPermit.create(date_issued: "2015-04-06 11:38:17", permit_number: "324", address: "2060 North Jasmine Street", valuation: 1, fee: 34, owner_name: "JASMINE", contractor_name: "besine", permit_type: permit, latitude: 39.7455399339223, longitude: -104.92825913894)
         near = BuildingPermit.near('2035 N JASMINE ST, Denver Colorado')
 
         visit '/'
