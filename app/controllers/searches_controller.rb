5fd289e2d79a8b3cf04f58b8267d35d4a282dd5e
Megan Arellano <ssciolist@gmail.com>
2018-05-24 20:12:00 -0600

    completes BP search refactor

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,11 +2,8 @@ class SearchesController < ApplicationController
   def index
     @presenter = BuildingPermitSearchPresenter.new
     @search = BuildingPermit.search_result(params[:days], params[:search], params[:distance])
-    # @search = BuildingPermit.where(date_issued: (DateTime.now - params[:days].to_i)..(DateTime.now)).near(params[:search], params[:distance])
     @value = @search.sum(:valuation)
     @distance = params[:distance]
     @days = params[:days]
-
-
   end
 end
