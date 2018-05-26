c478637d142d34f747b7345a3df8b78b442bc5c3
Megan Arellano <ssciolist@gmail.com>
2018-05-25 19:29:42 -0600

    builds framework for seeding all base permit files

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,31 +14,33 @@ CSV.foreach('./data/permit-type-legend.csv', headers: true, header_converters: :
                      p_type_3: row[:p_type_3])
 end
 
-CSV.foreach('./data/April-2015_test.csv', headers: false) do |row|
-  permit_na = "999"
-  na = "(Not specified)"
-  BuildingPermit.create!(date_issued:     row[0],
-                         permit_number:   row[3],
-                         address:         row[6],
-                         sublocation:     row[8],
-                         building_class:  row[11],
-                         valuation:       row[15],
-                         fee:             row[16],
-                         owner_name:      (if row[18].present?
-                                              row[18]
-                                           else
-                                              na
-                                           end),
-                         contractor_name: (if row[20].present?
-                                              row[20]
-                                           else
-                                              na
-                                           end),
-                         permit_type_id:  (if row[22].present?
-                                              row[22]
-                                           else
-                                              permit_na
-                                           end)
-                       )
-  puts row[0]
+Dir.glob('data/base/*').each do |file|
+  CSV.foreach("./#{file}", headers: false) do |row|
+    permit_na = "999"
+    na = "(Not specified)"
+    BuildingPermit.create!(date_issued:     row[0],
+                           permit_number:   row[3],
+                           address:         row[6],
+                           sublocation:     row[8],
+                           building_class:  row[11],
+                           valuation:       row[15],
+                           fee:             row[16],
+                           owner_name:      (if row[18].present?
+                                                row[18]
+                                             else
+                                                na
+                                             end),
+                           contractor_name: (if row[20].present?
+                                                row[20]
+                                             else
+                                                na
+                                             end),
+                           permit_type_id:  (if row[22].present?
+                                                row[22]
+                                             else
+                                                permit_na
+                                             end)
+                         )
+    puts row[0]
+  end
 end
