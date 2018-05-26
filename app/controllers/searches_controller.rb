04d3bd6df632fae12467cfa9d57c44b1e4581144
Megan Arellano <ssciolist@gmail.com>
2018-05-26 13:14:39 -0600

    creates free user tier search again: user is limited to searching within the last 365 days and within 5 miles. Still need to implement radio boxes for search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,7 @@ class SearchesController < ApplicationController
   def index
     @presenter = BuildingPermitSearchPresenter.new
     @search = BuildingPermit.search_result(params[:days], params[:search], params[:distance])
+    @address = params[:search]
     @value = @search.sum(:valuation)
     @distance = params[:distance]
     @days = params[:days]
--- spec/features/user_can_search_permits_spec.rb
+++ spec/features/user_can_search_permits_spec.rb
@@ -7,7 +7,7 @@ describe 'As a visitor,' do
         permit = PermitType.create(name: "New home", p_type: "003")
         BuildingPermit.create(date_issued: "2018-05-22 11:38:17", permit_number: "24", address: "2035 North Jasmine Street", valuation: 770689, fee: 4, owner_name: "JASMINE", contractor_name: "besine", permit_type: permit, latitude: 39.748276, longitude: -104.918953)
         BuildingPermit.create(date_issued: "2018-05-22 11:38:17", permit_number: "324", address: "2060 North Jasmine Street", valuation: 1, fee: 34, owner_name: "JASMINE", contractor_name: "besine", permit_type: permit, latitude: 39.7455399339223, longitude: -104.92825913894)
-binding.pry
+
         visit '/'
 
         fill_in 'search', with: '2035 N JASMINE ST, Denver Colorado'
@@ -23,19 +23,20 @@ binding.pry
 
   describe 'when I am on a search result page' do
     it 'gives me options to change my search' do
-      # type = PermitType.create(name: "New home", p_type: "003")
-      permit = create(:building_permit)
+      type = PermitType.create(name: 'Single family, detached', p_type: "003")
+      permit = create_list(:building_permit, 4, permit_type: type)
 
       visit '/'
 
-      fill_in 'search', with: '2035 N JASMINE ST, Denver Colorado'
+      address = '2035 N JASMINE ST, Denver Colorado'
+      fill_in 'search', with: address
       click_on 'Search'
 
-      expect(page).to has_select?('search[days]')
-      expect(page).to has_select?('search[miles]')
+      expect(page).to have_content('Refine your search')
+      expect(page).to have_field('search', placeholder: address)
+      expect(page).to have_field('distance')
+      expect(page).to have_field('days')
       expect(page).to have_field('Single family, detached', checked: false)
-      expect(page).to have_field('Two family dwellings', checked: false)
-
     end
   end
 end
