74ea7d8af5d878f5fd43f24356115451db3984c8
Megan Arellano <ssciolist@gmail.com>
2018-05-26 19:53:50 -0600

    refactors test, but failing on ambiguous element that I don't understand

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -7,14 +7,14 @@ describe 'As a visitor' do
       it 'creates a new account' do
         visit root_path
         click_on "Sign up"
-        fill_in "user[username]", with: "Paul"
+        save_and_open_page
+        fill_in "user[email]", with: "PaulKarolyi@gmail.com"
         fill_in "user[password]", with: "Karolyi"
-        fill_in "user[confirm_password]", with: "Karolyi"
-        click_on "Sign Up"
+        fill_in "user[password_confirmation]", with: "Karolyi"
+        click_on "Sign up"
 
         expect(current_path).to eq(dashboard_path)
-        expect(page).to have_content("Account created successfully")
-        expect(page).to have_content("Logged in as Paul")
+        expect(page).to have_content("Welcome! You have signed up successfully.")
         expect(page).to_not have_content("Login")
         expect(page).to have_content("Logout")
       end
