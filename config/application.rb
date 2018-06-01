451803fa6e0d3644a10de4957af2f1c8e0cf5b22
Megan Arellano <ssciolist@gmail.com>
2018-05-31 19:16:49 -0600

    removes raise_in_transactional_callbacks

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,6 @@ module HelloBlock
       authentication:       'plain',
       enable_starttls_auto: true
     }
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+
   end
 end
