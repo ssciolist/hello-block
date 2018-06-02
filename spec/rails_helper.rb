7199807767e6ebe38ab82e30da10ab68dc4dc81e
Megan Arellano <ssciolist@gmail.com>
2018-06-02 14:33:37 -0600

    configures webmock to allow google maps api requests for geocoder

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,7 +16,8 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-# require 'webmock/rspec'
+require 'webmock/rspec'
+WebMock.disable_net_connect!(allow: 'https://maps.googleapis.com')
 # require 'vcr'
 #
 # VCR.configure do |config|
