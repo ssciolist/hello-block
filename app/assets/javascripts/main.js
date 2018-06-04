5f972e8627fe09431492bea9093ffdca730a30a6
Megan Arellano <ssciolist@gmail.com>
2018-06-04 08:37:44 -0600

    map can render location and show info on click

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -26,13 +26,43 @@ function geolocate() {
 }
 
 function initMap() {
-  // The location of Uluru
+
   var start_lat = parseFloat(document.getElementById('map').dataset.start_lat)
   var start_long = parseFloat(document.getElementById('map').dataset.start_long)
+  var permits = JSON.parse(document.getElementById('map').dataset.permits)
+
+
   var start_location = {lat: start_lat, lng: start_long};
   // The map, centered at Uluru
   var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 16, center: start_location});
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: start_location, map: map});
+  buildMarkers(permits, map)
+}
+
+
+const buildMarkers = (permits, map) => {
+  for (var i = 0; i < permits.length; i++) {
+    var permit = permits[i];
+    var latLng = new google.maps.LatLng(permit.latitude,permit.longitude);
+    var marker = new google.maps.Marker({
+      position: latLng,
+      icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 5,
+            fillColor: 'red',
+          },
+      map: map
+    });
+    buildInfoWindow(marker, permit)
+  }
+}
+
+const buildInfoWindow = (marker, data) => {
+  var infoWindow = new google.maps.InfoWindow();
+  google.maps.event.addListener(marker, "click", function(e) {
+    infoWindow.setContent(data.address);
+    infoWindow.open(map, marker)
+  })
 }
--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,6 +2,7 @@ class MapsController < ApplicationController
   def index
     @home_marker = Geocoder.coordinates(params[:search])
     @presenter ||= BuildingPermitSearchPresenter.new(params[:days], params[:search], params[:distance])
+
   end
 
 end
