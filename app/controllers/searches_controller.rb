8682da2b971a0efb9500075df03c7f6aa86e5fd7
Megan Arellano <ssciolist@gmail.com>
2018-06-06 08:02:40 -0600

    moves pagination links to the center of the page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,6 @@
 class SearchesController < ApplicationController
   def index
     @presenter ||= BuildingPermitSearchPresenter.new(params[:days], params[:search], params[:distance])
-    @search = Kaminari.paginate_array(@presenter.search_results).page(params[:page]).per(10)
+    @search = Kaminari.paginate_array(@presenter.search_results).page(params[:page])
   end
 end
