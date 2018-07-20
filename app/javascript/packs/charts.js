748143d49c5fac2af341e44db35fc0738ae80ae1
Megan Arellano <ssciolist@gmail.com>
2018-07-20 16:24:23 -0600

    Adds a dummy line line chart on the index page

---
+++ app/javascript/packs/charts.js
@@ -0,0 +1,29 @@
+import Chart from 'chart.js';
+
+let residential = document.getElementById("resConstruction");
+let resChart = new Chart(residential, {
+    type: 'line',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
