87103113c7b5d63318020854a38cc43a656fb2df
Megan Arellano <ssciolist@gmail.com>
2018-07-20 17:27:11 -0600

    adds grid layout to the homepage, configures two dummy charts to sit on the homepage

--- app/javascript/packs/charts.js
+++ app/javascript/packs/charts.js
@@ -27,3 +27,31 @@ let resChart = new Chart(residential, {
         }
     }
 });
+
+let commercial = document.getElementById("commConstruction");
+let commChart = new Chart(commercial, {
+    type: 'line',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
