8488f98b5a32ee332558d66afa5429b7f5d5e17f
Megan Arellano <ssciolist@gmail.com>
2018-07-21 17:56:33 -0600

    changes fetch address from local host to heroku

--- app/javascript/packs/charts.js
+++ app/javascript/packs/charts.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 let residential = document.getElementById("resConstruction");
 
 function drawResChart() {
-  return fetch(`http://localhost:3000/api/v1/building_permits/summarize?class=RESCON&years=2015,2016,2017,2018&group=years`)
+  return fetch(`https://hello-block.herokuapp.com/api/v1/building_permits/summarize?class=RESCON&years=2015,2016,2017,2018&group=years`)
   .then((response) => response.json())
   .then((rawData) => {
     resChart.data.datasets[0].data = [rawData['2015'], rawData['2016'], rawData['2017'], rawData['2018']]
@@ -59,7 +59,7 @@ let resChart = new Chart(residential, {
 let commercial = document.getElementById("commConstruction");
 
 function drawCommChart() {
-  return fetch(`http://localhost:3000/api/v1/building_permits/summarize?class=COMMCON&years=2015,2016,2017,2018&group=years`)
+  return fetch(`https://hello-block.herokuapp.com/api/v1/building_permits/summarize?class=COMMCON&years=2015,2016,2017,2018&group=years`)
   .then((response) => response.json())
   .then((rawData) => {
     commChart.data.datasets[0].data = [rawData['2015'], rawData['2016'], rawData['2017'], rawData['2018']]
