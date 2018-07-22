32c9c5eb4b70c8140c760378cf3115d2e031cd1b
Megan Arellano <ssciolist@gmail.com>
2018-07-22 17:51:31 -0600

    adds neighborhoods to map, all currently shaded the same color

--- app/javascript/packs/hb-maps.js
+++ app/javascript/packs/hb-maps.js
@@ -1,6 +1,5 @@
 import mapboxgl from 'mapbox-gl';
 
-
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXJlbGxhbm9tZWdhbiIsImEiOiJjamp4Nmp5ZzYwd3o2M3dwYXA1Z2hpOG9jIn0.t9gtewJ5FXvJl9F6skfdPg'
 
 let neighborhoodMap = new mapboxgl.Map({
@@ -8,5 +7,26 @@ let neighborhoodMap = new mapboxgl.Map({
   style: 'mapbox://styles/mapbox/streets-v9',
   center: [-104.962262, 39.737309],
   zoom: 10.5,
-  zoomControl: true
 })
+
+let nav = new mapboxgl.NavigationControl();
+neighborhoodMap.addControl(nav, 'top-right');
+
+neighborhoodMap.on('load', function () {
+  neighborhoodMap.addSource('nbhdPolygons', {
+    type: 'geojson',
+    data: 'https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/denver.geojson'
+  });
+
+  neighborhoodMap.addLayer({
+    id: 'nbhdOutlines',
+    type: 'fill',
+    source: 'nbhdPolygons',
+    layout: {
+      visibility: 'visible'
+    },
+    paint: {
+      'fill-color': 'rgba(61,153,80,0.55)'
+    }
+  });
+});
