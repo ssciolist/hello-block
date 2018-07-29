6562ac33ca98b802aa3f845124c42aa5a89c7a4e
Megan Arellano <ssciolist@gmail.com>
2018-07-28 21:53:42 -0600

    fixes manually created geom column with migration.

--- db/migrate/20180729035047_add_geom_to_building_permits.rb
+++ db/migrate/20180729035047_add_geom_to_building_permits.rb
@@ -1,5 +1,5 @@
 class AddGeomToBuildingPermits < ActiveRecord::Migration[5.2]
   def change
-    add_column :building_permits, :geom, :point, :srid => 4326
+    add_column :building_permits, :geom, :st_point, :srid=>4326
   end
 end
--- spec/models/neighborhood_spec.rb
+++ spec/models/neighborhood_spec.rb
@@ -9,14 +9,8 @@ RSpec.describe Neighborhood, type: :model do
   context 'class methods' do
     context '.summarize_valuation' do
       it 'should return an activerecord relation with sum field equal to valuaution total' do
-        CSV.foreach('./data/denver_nbhds_binary.csv', headers: true, header_converters: :symbol) do |row|
-          Neighborhood.create!(
-                             name:   row[:nbhd_name],
-                             geom: row[:string_wkb]
-                             )
-        end
         type = PermitType.create(name: 'Single family, detached', p_type: '003')
-        create(:building_permit, permit_number: '2018-COMMCON-1', address: "6301 East 17th Avenue Parkway", valuation: 90, permit_type: type)
+        create(:building_permit)
         require 'pry'; binding.pry
 
 
