e84790522bf0592e114a28b3752bf4e14ac8a367
Megan Arellano <ssciolist@gmail.com>
2018-07-30 14:47:44 -0600

    Adds a cache expiration to the api page

--- app/controllers/api/v1/building_permits/neighborhoods/sum_controller.rb
+++ app/controllers/api/v1/building_permits/neighborhoods/sum_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::BuildingPermits::Neighborhoods::SumController < ApplicationController
-  caches_action :show, :cache_path => Proc.new {|c| c.request.url }
+  caches_action :show, expires_in: 100.hour, :cache_path => Proc.new {|c| c.request.url }
 
   def show
     render json: NeighborhoodPresenter.new(permit_class, years).geojson_summary
