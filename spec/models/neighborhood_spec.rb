2681da8dd65ead08c9b69b88230c6fb587ce82ce
Megan Arellano <ssciolist@gmail.com>
2018-07-30 17:28:01 -0600

    fixes geometry setting, tests still incorrect

--- spec/models/neighborhood_spec.rb
+++ spec/models/neighborhood_spec.rb
@@ -10,10 +10,14 @@ RSpec.describe Neighborhood, type: :model do
     context '.summarize_valuation' do
       it 'should return an activerecord relation with sum field equal to valuaution total' do
         type = PermitType.create(name: 'Single family, detached', p_type: '003')
-        create(:building_permit)
-        require 'pry'; binding.pry
-
-
+        permit = create(:building_permit, permit_type: type)
+        CSV.foreach('./db/denver_nbhds_binary.csv', headers: true, header_converters: :symbol) do |row|
+          Neighborhood.create!(
+                             name:   row[:nbhd_name],
+                             geom: row[:string_wkb]
+                             )
+        end
+    require 'pry'; binding.pry
         expect(Neighborhood.summarize_valuation(['2018%']).length).to eq(1)
         expect(Neighborhood.summarize_valuation(['2018%'])[0].sum).to eq(1)
       end
